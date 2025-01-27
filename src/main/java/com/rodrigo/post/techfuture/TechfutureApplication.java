62c0168b2e327638630fcb58cfe5ca70a1ed21b4
Rodrigo Castro <rodrigocd28@gmail.com>
2025-01-27 17:10:08 -0300

    Subindo o projeto no git

---
+++ src/main/java/com/rodrigo/post/techfuture/TechfutureApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.post.techfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechfutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/post/techfuture/TechfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.post.techfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
